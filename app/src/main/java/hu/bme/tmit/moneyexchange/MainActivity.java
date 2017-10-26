a42ea83ec17d7d7ff8556f52b22de76608baf70a
Maximilian Peters <de.maxptrs@googlemail.com>
2017-10-26 13:54:26 +0200

    added money image button

--- app/src/main/java/hu/bme/tmit/moneyexchange/MainActivity.java
+++ app/src/main/java/hu/bme/tmit/moneyexchange/MainActivity.java
@@ -1,13 +1,26 @@
 package hu.bme.tmit.moneyexchange;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.ImageButton;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    ImageButton money;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        money = findViewById(R.id.money);
+        money.setOnClickListener(MainActivity.this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        Intent i = new Intent(MainActivity.this, MoneyActivity.class);
+        startActivity(i);
     }
 }
